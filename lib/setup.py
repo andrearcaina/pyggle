1ed85ee499e16fb5cb12ce849af74eefbc6fde93
Andre <dtandre331@gmail.com>
2024-02-26 19:12:57 -0500

    setup

--- lib/setup.py
+++ lib/setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("../README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name='pyggle',
     version='0.0.1',
     author='Andre Arcaina',
     author_email='dtandre331@gmail.com',
     description='Python-based package that allows you to solve any N x M Boggle board.',
-    long_description='A Python library for solving Boggle boards (multiple different variations!)',
-    long_description_content_type='text/plain',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/andrearcaina/pyggle',
     py_modules=['boggle'],
     python_requires='>=3.6',
