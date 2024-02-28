eb7c0380e54d1702ede36617661883ea35936015
Andre Arcaina <dtandre331@gmail.com>
2024-02-28 11:41:08 -0500

    Update example_no_data.py

--- lib/examples/example_no_data.py
+++ lib/examples/example_no_data.py
@@ -3,7 +3,7 @@ from pyggle import Boggle
 if __name__ == "__main__":
     board = [['e', 'a'], ['s', 't']]
     
-    # given no words, will use 380k word list instead
+    # given no words, will use 479k word list instead
     boggle = Boggle(board)
 
     # print full dictionary
