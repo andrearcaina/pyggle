55c2322aaf6fe143482b3cfbca15879631bf458b
Andre <dtandre331@gmail.com>
2024-02-28 20:28:07 -0500

    1.0.3

--- lib/setup.py
+++ lib/setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup, find_packages
 from os import path
 
+# python setup.py sdist bdist_wheel
+# twine upload dist/pyggle-1.0.3.tar.gz
+
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, "README.md"), "r", encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
     name='pyggle',
-    version='1.0.2',
+    version='1.0.3',
     author='Andre Arcaina',
     author_email='dtandre331@gmail.com',
     description='Python-based package that allows you to solve any N x M Boggle board.',
