b0233e2f671b98bb0728f38023a076aca819b93d
Andre <dtandre331@gmail.com>
2024-03-05 22:28:44 -0500

    back to fastapi (#8)

--- backend/api/server.py
+++ backend/api/server.py
@@ -1,26 +1,18 @@
-from flask import Flask, jsonify, request
-from helpers.get_boggle import possibilities
-from helpers.detect_img import *
+from fastapi import FastAPI
+from typing import Union
+from api.helpers.get_boggle import possibilities as boggle_possibilities
+from api.helpers.detect_img import *
 
-app = Flask(__name__)
+app = FastAPI()
 
-@app.route("/api/words")
-def return_words():
-    board = request.args.get('board')
-    words = request.args.get('words') or None
+@app.get("/api/words")
+async def return_words(board: str, words: Union[str, None] = None):
+    return boggle_possibilities(board, True, words)
 
-    return jsonify(possibilities(board, True, words))
+@app.get("/api/coords")
+async def return_coords(board: str, words: Union[str, None] = None):
+    return boggle_possibilities(board, False, words)
 
-@app.route("/api/coords")
-def return_coords():
-    board = request.args.get('board')
-    words = request.args.get('words') or None
-
-    return jsonify(possibilities(board, False, words))
-
-@app.route("/api/detect")
-def return_image():
-    return jsonify({"image": "detection"})
-
-if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+@app.get("/api/detect")
+async def return_image():
+    return {"image": "detection"}
