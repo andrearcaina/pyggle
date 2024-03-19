ea215c75d0f1d5eccd9349692dbf72153d76c475
Andre <dtandre331@gmail.com>
2024-03-19 18:50:10 -0400

    qu None

--- lib/examples/qu_example.py
+++ lib/examples/qu_example.py
@@ -1,7 +1,7 @@
 from pyggle import Boggle, time
 
 if __name__ == "__main__":
-    boggle = Boggle("aqa dsf", [], True)
+    boggle = Boggle("aqa dsf", None, True)
 
     # the letter "q" now represents "qu" in official rules
     boggle.print_result()
