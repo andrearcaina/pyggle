037cd9c3a81193567e1ae723d29ef6857b28d838
Andre Arcaina <dtandre331@gmail.com>
2024-03-19 18:38:04 -0400

    commenting

--- lib/examples/4x4_example.py
+++ lib/examples/4x4_example.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     with open('sample_data.txt', 'r') as file:
         words = [line.strip() for line in file]
 
-    boggle = Boggle(board, words, True)# print time to solve boggle board
-
+    boggle = Boggle(board, words, True)
+    
+    # print time to solve boggle board
     print(time(boggle)) # roughly 0.01 seconds
 
     print(sum(score(boggle))) # 30
